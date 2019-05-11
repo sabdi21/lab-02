a7ec0fe0e49a640c5dcdfeb9ef12d123e3f8e85b
sabdi21 <sabdi21@hotmail.com>
2019-05-11 14:57:06 -0700

    nothing

--- js/app.js
+++ js/app.js
@@ -1,70 +1,119 @@
-const photosArray = [];
+// ============GLOBAL VARIABLES============================
 
-const keywordArray = [];
+// const photos = [];
 
-// construct a Photo given some information fetched externally
+// const keyword = [];
+
+// ================ ITEM REFERENCES STORED IN A CONSTRUCTOR================
 function Photo(image) {
   this.image_url= image.image_url; 
   this.title= image.title;
   this.description= image.description;
-  this.keyboard= image.keyboard;
+  this.keyword= image.keyword;
   this.horns = image.horns;
 }
 
-// adding photos to the DOM
-Photo.readJson = function () {
-
-  $.get(`data/page-1.json`, `json`) 
-    .then((jsonPhotos) => {
+//=================adding photos to the DOM=========================
+Photo.readJson = (page) => {
+  
+  $.get(`data/page-${page}.json`) 
+    .then(jsonPhotos => {
+  
+      // adds new photos to the photo constructor array====
+      Photo.all = []; 
       jsonPhotos.forEach(photo => {
-        photosArray.push.apply(new Photo(photo))
+        Photo.all.push(new Photo(photo))
       });
+  
       Photo.all.forEach(photo => {
-        $("photo").append(Photo.render());
+        $('main').append(photo.render());
       })
     });
+
+
+}
+// =================== RENDERS PHOTOS TO THE DOM ==================
+Photo.prototype.render = function() {
+  const clone = $(`#photo-container`).clone();
+  
+  clone.removeAttr(`id`);
+  clone.find(`h2`).text(this.title);
+  clone.find(`img`).attr(`src`, this.image_url);
+  clone.find(`img`).attr(`alt`, this.title);
+  clone.find(`p`).html(this.description);
+  clone.attr(`data-keyword`, this.keyword);
+
+  return clone;
 }
 
-Photo.prototype.render = function() {
-  let photoTemplate = Handlebars.compile('#photo-template').html();
+function keywordList() {
+  const keywordAarray= [];
+  // Photo.all = [];
+
+  // Photo.all.forEach(photo => {
+  //   keywordAarray.push(photo.keyword)
+  // });
+  // console.log(keywordAarray);
+  
+
+  const selectElement = $('#keyword-list').text();
+
+  selectElement.append("<option>");
+  // opt.removeAttr('id');
+  // opt.attr('value', 'test');
+  console.log($('select')[0])
+  $('select')[0].append(opt);
+  console.log('keyword list');
+
+  // keyword.all();
+}
+
+// Photo.prototype.render = function() {
+//   let photoTemplate = Handlebars.compile($('#photo').html());
  
-  return photoTemplate(this);
-};
+//   return photoTemplate(this);
+// };
+ 
+// }).then(() => {
 
-// do what it takes to add Photo to the DOM
-   
-function loadPhotoData() {
+// });
 
-//       // convert the raw photo objects into Photo instances
-//       // add each instance to photos array
-//       // for each Photo instance
-//       // 1) add the photo's keyword to keywords array, avoid duplicates
-//       // 2) "render" the photo to the screen
-      photosArray.forEach(item) 
-      const itemCheck = keywords.includes(keyword)
-
-
-//     }).then(() => {
 
 //       // populate <select> element with options
 //       // based on keywords array
 
 //     }).then(() => {
-
+//
 //       // wire up an event handler that will listen
 //       // for the 'change' event
 //       // and show only photos that match selected value
 //       // should show all if on 'default' selection
+// $('select').on('change', function() {
 
+//   const selectedValue = $(this).val();
+
+//   if(selectedValue === 'default') {
+//     $('section').show();
+
+//   } else {
+//     $('section').hide();{
+
+//     }
+
+//   }
+// })
 // });
 //       });
 //     });
 // }
 
+// ===== seting img width and height======
 
-// when jQuery says document is ready then call function to load photo data
+
+// ==========when jQuery says document is ready then call function to load photo data=========
 $(document).ready(function() {
-  Photo.readJson();
+  Photo.readJson(1);
+  keywordList();
 });
 
 
@@ -72,3 +121,4 @@ $(document).ready(function() {
 // $(() => {
 //   Photo.readJson();
 // });
+// }
